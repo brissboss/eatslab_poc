c2d71179baf09a18744c02389fbc3568bfba9974
Theo Brissiaud <theobrissiaud@Theos-MacBook-Air.local>
2024-02-21 19:56:13 +0100

    test

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
-<template>
-  <v-app>
-    <v-main>
-      <HelloWorld />
-    </v-main>
-  </v-app>
-</template>
-
 <script setup lang="ts">
-  //
+import RouterPage from '@/page/RouterPage.vue'
+
+const partner = window.location.href.split('/')[window.location.href.split('/').length - 1]
+
 </script>
+
+<template>
+	<v-app>
+		<v-main>
+			  <RouterPage
+            :partner="partner"
+        />
+		</v-main>
+	</v-app>
+</template>
---
+++ src/page/RouterPage.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { defineProps, ref, onBeforeMount } from 'vue'
+
+const props = defineProps<{
+    partner: string
+}>()
+
+const json = ref('')
+
+onBeforeMount(() => {
+    fetchJson()
+})
+
+function fetchJson() {
+    fetch(`https://github.com/brissboss/eatslab_partner/blob/main/${props.partner}.json`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            json.value = JSON.stringify(data)
+        })
+}
+
+</script>
+
+<template>
+    {{ partner + json }}
+</template>
